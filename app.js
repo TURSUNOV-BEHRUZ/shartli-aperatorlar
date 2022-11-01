3fbfb183d4b1d5d5e695f5639819de0330f08f7f
BEHRUZBEK TURSUNOV <alexmeyson966@gmail.com>
2022-11-01 21:20:40 +0500

    shartli aperator

---
+++ app.js
@@ -0,0 +1,55 @@
+let son = prompt('KINO KURADIGAN YOSH') ;
+
+
+if (son >= 30){
+    alert('BAYEVIK KINO KURISH YOSHI');
+}else if(son >= 10 && son <= 30){
+    alert('KAMEDIAN KINO KURISH YOSHI');
+}else if(son > 3 && 15 > son){
+    alert('MULTIFILM KURISH YOSHI');
+}else{
+    alert('TELEVIZOR KURISH TAQIQLANADI');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
